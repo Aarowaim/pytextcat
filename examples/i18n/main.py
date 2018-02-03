a9aa4f360d7c1dddd1480b65e7b38d2c07e1b93f
Aaron Lip <kitsunay34@gmail.com>
2018-02-03 14:24:51 -0700

    Cleanup
    Examples moved to folders
    
    i18n translation example now is also runnable from different directories
    
    More package metadata in setup.py

--- examples/i18n/main.py
+++ examples/i18n/main.py
@@ -5,10 +5,13 @@ it becomes possible to swap out text simply by loading a different file.
 '''
 
 from pytextcat import TextCatalog
+import os
 
 
 def hello_world(language_code):
-    cat = TextCatalog('{}.trans'.format(language_code))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    fname = os.path.join(current_dir, '{}.trans'.format(language_code))
+    cat = TextCatalog(fname)
 
     name = input(cat['prompt'])
     print(cat['greeting'].format(name))
--- setup.py
+++ setup.py
@@ -4,4 +4,14 @@ setup(name='pytextcat',
       version='0.1',
       description='Python Text Catalogs',
       author='Aaron L',
-      py_modules=['pytextcat'])
+      url='https://github.com/Aarowaim/pytextcat',
+      download_url='https://github.com/Aarowaim/pytextcat/archive/master.zip',
+      py_modules=['pytextcat'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: User Interfaces'
+      ])
